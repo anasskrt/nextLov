e907fcf94666f244efaa3bd4fb4c709e184ac814
Anass Kerchaoui <anass.kerchaoui@efrei.net>
2025-06-17 17:44:58 +0200

    fix header

--- front/next.config.ts
+++ front/next.config.ts
@@ -1,14 +1,17 @@
-import type { NextConfig } from "next";
 import path from "path";
+import type { NextConfig } from "next";
+import type { Configuration as WebpackConfig } from "webpack";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   images: {
     domains: ["images.unsplash.com"],
   },
-  webpack: (config) => {
+  webpack: (config: WebpackConfig) => {
     config.resolve = config.resolve || {};
     config.resolve.alias = config.resolve.alias || {};
-    config.resolve.alias['@'] = path.resolve(__dirname);
+    // 👇👇 Cast ici !
+    (config.resolve.alias as { [key: string]: string })['@'] = path.resolve(process.cwd());
     return config;
   },
 };
