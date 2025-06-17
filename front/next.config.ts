2abe797a0f96fe0f91aaaf9f891b8d06fa23f90b
Anass Kerchaoui <anass.kerchaoui@efrei.net>
2025-06-17 17:29:42 +0200

    next config

--- front/next.config.ts
+++ front/next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   images: {
     domains: ["images.unsplash.com"],
+  },
+  webpack: (config) => {
+    config.resolve = config.resolve || {};
+    config.resolve.alias = config.resolve.alias || {};
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
   },
 };
 
